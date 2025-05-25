96bfeccf0a6ed422264d3c68ed99a9743d39434f
alexandre11aa <alexandrestrela@hotmail.com>
2025-05-25 13:21:52 -0300

    :recycle: refact: Reconfigurando allowed hosts

--- backend/rainscale/core/settings.py
+++ backend/rainscale/core/settings.py
@@ -26,12 +26,10 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'change-me')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.getenv('DEBUG', 0)))
 
-# ALLOWED_HOSTS = [
-#     h.strip() for h in os.getenv('ALLOWED_HOSTS', '').split(',')
-#     if h.strip()
-# ]
-
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    h.strip() for h in os.getenv('ALLOWED_HOSTS', '').split(',')
+    if h.strip()
+]
 
 
 # Application definition
